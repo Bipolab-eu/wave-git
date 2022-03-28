8147227172c1d9a15cbef9b2080f8361fb1abd99
Bentagai <bentagai@protonmail.com>
2022-03-28 14:55:12 +0100

    sube archivos

---
+++ wave-extended.js
@@ -0,0 +1,26 @@
+var yoff = 0.0
+let escala = 50
+
+function setup () {
+  createCanvas(24 * escala, 8 * escala)
+}
+
+function draw() {
+  background('#00629B')
+  fill('#FFB600')
+  noStroke()
+  beginShape()
+  
+  var xoff = 0
+  yoff += 0.002 // si se comenta, detiene el movimiento
+  for (let y = 0; y <= height; y += 4) {
+    var value = noise(xoff, yoff)
+    let x = map(value, 0, 1, 0, width)
+    vertex(x, y)
+    xoff += 0.01
+  }
+
+  vertex(0, height) // Punto A
+  vertex(0, 0) // Punto B
+  endShape()
+}
